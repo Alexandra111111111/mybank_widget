18dfe7a1b1e409e3c7f69c2d2c24bf4880e4426f
Alexandra <alex5011993@yandex.ru>
2025-05-09 15:24:06 +1000

    Создан файл processing и реализованы функции filter_by_state, sort_by_date

---
+++ src/processing.py
@@ -0,0 +1,24 @@
+from typing import List, Dict, Optional
+
+def filter_by_state(data_list: List[Dict[str, str]], state: str = 'EXECUTED') -> List[Dict[str, str]]:
+    """
+    Фильтрует список словарей по значению ключа 'state'
+    """
+    result = []
+    for item in data_list:
+        if item.get('state') == state:
+            result.append(item)
+    return result
+
+def sort_by_date(data_list: List[Dict[str, str]]) -> List[Dict[str, str]]:
+    """
+    Фильтрует список словарей по значению ключа 'date'
+    """
+    sorted_data = sorted(data_list, key=lambda x: x['date'], reverse=True)
+    return sorted_data
+
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+print([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])
+
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+print([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])
