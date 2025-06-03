62e0b0d55c9003b23fe5d17675d8f979e36ba60c
Alexandra <alex5011993@yandex.ru>
2025-06-03 22:57:48 +1000

    еализованы функция filter_by_currency, функция-генератор transaction_descriptions и генератор card_number_generator в модуле generators.py

--- src/generators.py
+++ src/generators.py
@@ -10,3 +10,26 @@ def filter_by_currency(transactions: List[Dict], currency_code: str) -> Iterator
     """
     return (tx for tx transactions if tx["operationAmount"]["currency"]["code"] == currency_code)
 
+
+def transaction_descriptions(transactions: List[Dict]) -> Iterator[str]:
+    """
+    Генерирует описания транзакций последовательно.
+
+    :param transactions: Список словарей с данными транзакций.
+    :return: Последовательность строк с описанием каждой транзакции.
+    """
+    for tx in transactions:
+        yield tx.get("description", "")
+
+
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
+    """
+    Генератор, выдающий номера банковских карт в виде формата 'XXXX XXXX XXXX XXXX'.
+
+    :param start: Начальное значение последовательности.
+    :param stop: Конечное значение последовательности.
+    :return: Следующий номер банковской карты в формате 'XXXX XXXX XXXX XXXX'.
+    """
+    for number in range(start, stop + 1):
+        formatted_card = f"{number:016d}"
+        yield " ".join(formatted_card[i:i + 4] for i in range(0, len(formatted_card), 4))
